c4cfbb5a3b5533538c71239073cd482251b6a790
Charlie White <charlie@triaged.co>
2015-01-12 15:43:22 -0500

    waves serializer

--- app/serializers/wave_serializer.rb
+++ app/serializers/wave_serializer.rb
@@ -1,5 +1,5 @@
 class WaveSerializer < ApplicationSerializer
-  attributes :id, :title, :slug
+  attributes :id, :title, :slug, :latitude, :longitude
 
   def slug
   	object.slug.underscore.humanize if object.slug 
