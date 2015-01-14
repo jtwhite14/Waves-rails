6b62a3aa0d9c778342b7c0945e76f52c526b91ea
Charlie White <charlie@triaged.co>
2015-01-14 14:20:44 -0500

    create wave fix

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -14,7 +14,7 @@ end
 # end
 
 # Stop active_model_serializers from adding root elements to JSON responses.
-# ActiveModel::Serializer.root = false
+ActiveModel::Serializer.root = false
 ActiveModel::ArraySerializer.root = false
 
 
