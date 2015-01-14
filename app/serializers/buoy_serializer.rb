437d4be783f12231469cda70faa1ee6b5ffebf6b
Charlie White <charlie@triaged.co>
2015-01-14 10:35:29 -0500

    buoy defaults

--- app/serializers/buoy_serializer.rb
+++ app/serializers/buoy_serializer.rb
@@ -2,4 +2,8 @@ class BuoySerializer < ApplicationSerializer
   attributes :id, :title, :description, :latitude, :longitude, :station_id
 
   has_one :current_observation
+
+  def description
+  	object.description ? object.description : ""
+  end
 end
