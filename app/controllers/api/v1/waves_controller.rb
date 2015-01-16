008f87c4dd54098a16e98a2db0e496786525ab48
Charlie White <charlie@triaged.co>
2015-01-15 20:07:45 -0500

    buoy auth

--- app/controllers/api/v1/waves_controller.rb
+++ app/controllers/api/v1/waves_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::WavesController < APIController
 
 	def index
-		@waves = Wave.all
+		@waves = current_user.waves.all
 		if (params[:latitude] && params[:longitude])
 			@waves = DistanceCollection.new(@waves).set_distance_from([params[:latitude], params[:longitude]]).sort_by(&:distance)
 			@waves = @waves.take(params[:limit].to_i) if params[:limit]
