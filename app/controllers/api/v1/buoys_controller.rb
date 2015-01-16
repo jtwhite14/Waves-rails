7649d73fb1ee336b580fc2205e225df54677154d
Charlie White <charlie@triaged.co>
2015-01-16 13:39:18 -0500

    expires

--- app/controllers/api/v1/buoys_controller.rb
+++ app/controllers/api/v1/buoys_controller.rb
@@ -3,8 +3,11 @@ class Api::V1::BuoysController < APIController
 	skip_before_filter :authenticate_user!, :except => [:create]
 
 	def index
-		@buoys = Buoy.page(params[:page]).per(5).includes(:current_observation)
-		respond_with @buoys, serializer: PaginationSerializer
+		# @buoys = Buoy.page(params[:page]).per(5).includes(:current_observation)
+		# respond_with @buoys, serializer: PaginationSerializer
+		@buoys = Buoy.all.includes(:current_observation)
+		expires_in 1.hour, :public => true
+		respond_with @buoys
 	end
 
 
