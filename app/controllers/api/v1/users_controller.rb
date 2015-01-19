1352b51be1393329a2996bb5982ab5e8c48fcafb
Charlie White <charlie@triaged.co>
2015-01-19 17:45:22 -0500

    serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < APIController
   def create
   	@user = current_user
   	@user.update(user_params)
-  	respond_with @user
+  	respond_with @user, location: me_api_v1_users_path
   end
 
 private
