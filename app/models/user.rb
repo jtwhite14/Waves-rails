fcd1500cb6d5822ba461a5fa4f3e4fd777a6f953
Charlie White <charlie@triaged.co>
2015-01-19 16:56:39 -0500

    avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :sessions
 
   before_save :ensure_authentication_token!
-  mount_uploader :avatar, AvatarPhotoUploader
+  mount_uploader :avatar, AvatarUploader
   process_in_background :avatar
 
 
