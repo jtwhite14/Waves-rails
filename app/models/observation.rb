e700edf25cf01afc053189aa9c72184119c82c5b
Charlie White <charlie@triaged.co>
2015-01-20 15:50:44 -0500

    fixing import timestamps

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -15,7 +15,7 @@ class Observation < ActiveRecord::Base
   	b = BuoyData::NoaaBuoyObservation.new(buoy.station_id)
   	unless (b.get == nil)
         observation = Observation.create(
-      		timestamp: DateTime.new(b.YY.to_i, b.MM.to_i, b.DD.to_i, b.hh.to_i, b.mm.to_i, 0),
+      		timestamp: DateTime.new(b.YY.to_i, b.MM.to_i, b.DD.to_i, b.hh.to_i, 0, 0),
       		wave_height: b.WVHT,
       		swell_height: b.SwH,
       		swell_period: b.SwP,
