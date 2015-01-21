81b370ce00b05774ab4b0cb1fe1400517c5bd2cf
Charlie White <charlie@triaged.co>
2015-01-21 14:34:34 -0500

    avatar url or nil

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ApplicationSerializer
   attributes :id, :authentication_token, :name, :email, :avatar_url
 
   def avatar_url
-  	object.avatar.thumb.url
+  	object.avatar.thumb.url ? object.avatar.thumb.url : "" 
   end
 
 end
