1b979f35d342b17a049fdfbef349c1aa82ae45fe
Charlie White <charlie@triaged.co>
2015-01-25 21:10:23 -0800

    adding wave validation for sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,9 +3,9 @@ class Session < ActiveRecord::Base
   belongs_to :wave
   belongs_to :observation
   counter_culture :wave
-
-  mount_uploader :session_photo, SessionPhotoUploader
+	mount_uploader :session_photo, SessionPhotoUploader
   
-
   default_scope { where(finalized: true) } 
+
+  validates :wave, :presence => true
 end
